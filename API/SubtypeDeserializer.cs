08e89ecbd320ca31a28e5b7809e769621901fb85
Raymond Johnson <rayzerbrain@gmail.com>
2022-02-18 23:42:39 -0600

    WORK NOW AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa

---
+++ API/SubtypeDeserializer.cs
@@ -0,0 +1,76 @@
+﻿using Exiled.API.Features;
+using Exiled.Loader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemUtils.API
+{
+    //T is the base class
+    public class SubtypeDeserializer<TBase> where TBase : class
+    {
+        // Finds highest clas in the hierarchy(T being highest) that retains all properties from the config
+        public TBase FindSmallestSubtype(string rawConfig, List<Type> types)
+        {
+            TBase baseObj = null;
+
+            types.RemoveAll((t) => !t.IsSubclassOf(typeof(TBase)) && !t.Equals(typeof(TBase)));
+            
+            foreach(Type t in types)
+            {
+                if (TryDeserialize(rawConfig, t, out TBase newObj))
+                {
+                    if (baseObj == null || baseObj.GetType().IsSubclassOf(t))
+                    {
+                        baseObj = newObj;
+                        Log.Debug($"Deserialized object type becoming new type {t}", PluginMain.Instance.Config.DebugMode);
+                    }
+                }
+            }
+            Log.Debug($"Smallest substype was {baseObj.GetType()}");
+            return baseObj;
+        }
+
+        private bool TryDeserialize(string rawConfig, Type subType, out TBase mod)
+        {
+            mod = (TBase)Loader.Deserializer.Deserialize(rawConfig, subType);
+            string allProps = Loader.Serializer.Serialize(mod) + "\n";
+
+            // If any property in the serialized config is not found in the full list of properties, deserialization marked as unsuccessfull
+            foreach (string prop in GetRawProperties(rawConfig))
+            {
+                if (!allProps.Contains(prop))
+                {
+                    Log.Debug($"{allProps} was not {subType} because of {prop}", PluginMain.Instance.Config.DebugMode);
+                    mod = null;
+                    return false;
+                }
+            }
+            Log.Debug("SUCCESSSSSSSSSSSSSSSSSSSSSSSS");
+            return true;
+        }
+        private List<string> GetRawProperties(string rawConfig)
+        {
+            string[] lines = rawConfig.Split('\n');
+            StringBuilder prop = new StringBuilder();
+            List<string> props = new List<string>();
+
+            prop.AppendLine(lines[0].Substring(0, lines[0].Length - 1));
+            for (int i = 1; i < lines.Length - 1; i++)
+            {
+                char c = lines[i][0];
+                if (c != ' ' && c != '-')
+                {
+                    //prop.Remove(prop.Length - 1, 1);
+                    props.Add(prop.ToString());
+                    prop.Clear();
+                }
+                prop.AppendLine(lines[i].Substring(0, lines[i].Length-1));
+            }
+            props.Add(prop.ToString());
+            return props;
+        }
+    }
+}
--- PluginMain.cs
+++ PluginMain.cs
@@ -8,6 +8,7 @@ using Exiled.API.Features;
 using Exiled.Loader;
 
 using ItemUtils.API.Modifiers;
+using ItemUtils.API;
 
 namespace ItemUtils
 {
@@ -20,6 +21,7 @@ namespace ItemUtils
         public override string Author => "rayzer";
         public override string Name => "ItemUtils";
         private List<ItemModifier> loadedModifiers;
+        private SubtypeDeserializer<ItemModifier> sd;
         private Harmony hrmny;
         public override void OnEnabled()
         {
@@ -41,8 +43,8 @@ namespace ItemUtils
         public void LoadModifiers()
         {
             loadedModifiers = new List<ItemModifier>();
-            ItemModifier mod = null;
-            string rawMod;
+            sd = new SubtypeDeserializer<ItemModifier>();
+            List<Type> types = new List<Type>(Assembly.GetTypes());
 
             foreach (KeyValuePair<ItemType, string> map in Config.ModifiedItems)
             {
@@ -52,25 +54,9 @@ namespace ItemUtils
                     continue;
                 }
 
-                foreach (Type t in Assembly.GetTypes())
-                {
-                    //if t.Equals(typeof(ItemModifier))
-                    if (t.IsSubclassOf(typeof(ItemModifier)) || t.Equals(typeof(ItemModifier)))
-                    {
-                        rawMod = Loader.Serializer.Serialize(match);
-                        //mod = (ItemModifier)Loader.Deserializer.Deserialize(rawMod, t);
-                        //if (Loader.Serializer.Serialize(mod).Equals(rawMod))
-                        //Log.Debug($"Testing config \n{rawMod}\n against type {t}", Config.DebugMode);
-                        if (TryDeserialize(rawMod, t, out mod))
-                        {
-                            Log.Debug($"Loading modifier of type {t} for item {map.Key} with definition name {map.Value}", Config.DebugMode);
-                            mod.Type = map.Key;
-                            mod.RegisterEvents();
-                            break;
-                        }
-                    }
-                }
+                ItemModifier mod = sd.FindSmallestSubtype(Loader.Serializer.Serialize(match), types);
                 Log.Assert(mod != null, $"Your config is not set up properly! Config:\n{map.Value}");
+                loadedModifiers.Add(mod);
             }
         }
         public void UnloadModifiers()
@@ -81,43 +67,6 @@ namespace ItemUtils
             }
             loadedModifiers = null;
         }
-        // This method checks the deserialized modifier to see if any properties are missing from the original configuration
-        public bool TryDeserialize(string rawConfig, Type t, out ItemModifier mod)
-        {
-            mod  = (ItemModifier)Loader.Deserializer.Deserialize(rawConfig, t);
-            string allProps = "\n" + Loader.Serializer.Serialize(mod);
-
-            // If any property in the serialized config is not found in the full list of properties, deserialization marked as unsuccessfull
-            foreach (string prop in GetRawProperties(rawConfig))
-            {
-                if (!allProps.Contains(prop))
-                {
-                    mod = null;
-                    return false;
-                }
-            }
-            return true;
-        }
-        public List<string> GetRawProperties(string rawConfig)
-        {
-            string[] lines = rawConfig.Split('\n');
-            StringBuilder prop = new StringBuilder();
-            List<string> props = new List<string>();
-
-            prop.AppendLine(lines[0]);
-            for (int i=1; i<lines.Length-1; i++)
-            {
-                char c = lines[i][0];
-                if (c != ' ' && c != '-')
-                {
-                    prop.Insert(0, "\n");
-                    props.Add(prop.ToString());
-                    prop.Clear();
-                }
-                prop.AppendLine(lines[i]);
-            }
-            props.Add(prop.ToString());
-            return props;
-        }
+        
     }
 }
