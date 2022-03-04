1e47bdd28205600b66b3b9a62b3ef3d46b2e6a94
Raymond Johnson <rayzerbrain@gmail.com>
2022-03-04 12:14:09 -0600

    Untested att. mods

--- API/Modifiers/FirearmModifier.cs
+++ API/Modifiers/FirearmModifier.cs
@@ -2,7 +2,11 @@
 
 using PlayerHandler = Exiled.Events.Handlers.Player;
 using ItemHandler = Exiled.Events.Handlers.Item;
-
+using InventorySystem.Items.Firearms.Attachments;
+using System.Collections.Generic;
+using ItemUtils.Events;
+using ItemUtils.Events.EventArgs;
+using Exiled.API.Features.Items;
 
 namespace ItemUtils.API.Modifiers
 {
@@ -12,17 +16,20 @@ namespace ItemUtils.API.Modifiers
         //all needs testing
         public bool NeedsAmmo { get; set; } = true;
         public bool CanDisarm { get; set; } = true;
+        public Dictionary<AttachmentNameTranslation, Dictionary<AttachmentParam, float>> ModifiedAttachments { get; set; } = new Dictionary<AttachmentNameTranslation, Dictionary<AttachmentParam, float>>();
         
         public override void RegisterEvents()
         {
             PlayerHandler.Handcuffing += OnHandcuffing;
             ItemHandler.ChangingDurability += OnUsingAmmo;
+            CustomHandler.ObtainingItem += OnObtainingItem;
             base.RegisterEvents();
         }
         public override void UnregisterEvents()
         {
             PlayerHandler.Handcuffing -= OnHandcuffing;
             ItemHandler.ChangingDurability -= OnUsingAmmo;
+            CustomHandler.ObtainingItem -= OnObtainingItem;
             base.UnregisterEvents();
         }
         public void OnUsingAmmo(ChangingDurabilityEventArgs ev)
@@ -38,6 +45,26 @@ namespace ItemUtils.API.Modifiers
                 return;
 
             ev.IsAllowed = CanDisarm;
+        }
+        public void OnObtainingItem(ObtainingItemEventArgs ev)
+        {
+            if (!CanModify(ev.Item, ev.Player))
+                return;
+
+            Firearm gun = ev.Item as Firearm;
+
+            foreach (FirearmAttachment att in gun.Attachments)
+            {
+                if (!ModifiedAttachments.ContainsKey(att.Name))
+                    continue;
+
+                for (int i = 0; i < att.Settings.SerializedParameters.Length; i++)
+                {
+                    if (ModifiedAttachments[att.Name].TryGetValue(att.Settings.SerializedParameters[i].Parameter, out float multi))
+                        att.Settings.SerializedParameters[i].Value *= multi;
+                          
+                }
+            }
         }
     }
 }
